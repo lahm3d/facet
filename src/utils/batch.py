4e24be5064f386ccac08e37efb1baa0a564809e2
Labeeb Ahmed <40400391+lahm3d@users.noreply.github.com>
2024-02-29 11:03:39 -0500

    Remove skip column from batch.csv and updates to batch processing

--- src/utils/batch.py
+++ src/utils/batch.py
@@ -1,15 +1,5 @@
 import pandas as pd
-from pathlib import Path
 
-def generate_processing_batch(batch_csv):
-    hucs = []
-    
+def generate_processing_batch(batch_csv):    
     batch = pd.read_csv(batch_csv, dtype=str)
-    process = batch.process.to_list()
-    skip = batch.skip.dropna().to_list()
-
-    for huc in process:
-        if huc not in skip:
-            hucs.append(huc)
-
-    return hucs 
+    return batch.process.to_list()
